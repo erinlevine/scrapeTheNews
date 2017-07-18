f815850c6bb07ace32d3d1b24326e481458cd12f
erinlevine <erinclevine@gmail.com>
2017-07-18 16:48:13 -0400

    Pushing Big Brother articles to the database and shows on the site

--- models/Article.js
+++ models/Article.js
@@ -8,12 +8,14 @@ var ArticleSchema = new Schema({
   // title is a required string
   title: {
     type: String,
-    required: true
+    required: true,
+    unique: true
   },
   // link is a required string
   link: {
     type: String,
-    required: true
+    required: true,
+    unique: true
   },
   // This only saves one note's ObjectId, ref refers to the Note model
   note: {
--- server.js
+++ server.js
@@ -30,7 +30,7 @@ app.use(bodyParser.urlencoded({
 app.use(express.static("public"));
 
 // Database configuration with mongoose
-mongoose.connect("mongodb://localhost/week18day3mongoose");
+mongoose.connect("mongodb://localhost/scraper");
 var db = mongoose.connection;
 
 // Show any mongoose errors
@@ -50,18 +50,20 @@ db.once("open", function() {
 // A GET request to scrape the echojs website
 app.get("/scrape", function(req, res) {
   // First, we grab the body of the html with request
-  request("http://www.echojs.com/", function(error, response, html) {
+  request("https://www.reddit.com/r/bigbrother/", function(error, response, html) {
     // Then, we load that into cheerio and save it to $ for a shorthand selector
     var $ = cheerio.load(html);
     // Now, we grab every h2 within an article tag, and do the following:
-    $("article h2").each(function(i, element) {
+     $("p.title").each(function(i, element) {
 
       // Save an empty result object
       var result = {};
 
       // Add the text and href of every link, and save them as properties of the result object
-      result.title = $(this).children("a").text();
-      result.link = $(this).children("a").attr("href");
+      result.title = $(element).children().text();
+      result.link = $(element).children().attr("href");
+      // result.summary = $("p.summary").text();
+
 
       // Using our Article model, create a new entry
       // This effectively passes the result object to the entry (and the title and link)
@@ -78,11 +80,10 @@ app.get("/scrape", function(req, res) {
           console.log(doc);
         }
       });
-
-    });
   });
   // Tell the browser that we finished scraping the text
   res.send("Scrape Complete");
+});
 });
 
 // This will get the articles we scraped from the mongoDB
